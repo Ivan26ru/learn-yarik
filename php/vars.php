3cd630af63b326da070ce9b9dbc78ec9396f461c
Yarik <arikpisarenko72@gmail.com>
2025-11-18 21:29:30 +0300

    Добавить описание примеру

--- php/vars.php
+++ php/vars.php
@@ -1,7 +1,10 @@
 <?php
 
+echo "Площадь прямоугольника <br> ";
 
 $dlina = 7;
 $shirina = 5;
 $S = $dlina * $shirina;
-echo $S;
+echo "длина " . $dlina . "<br>";
+echo "ширина" . $shirina . "<br>";
+echo "площадь прямоугольника" . $S . "<br>";
