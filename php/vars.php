6a3db5b3a2233de3d59b5e8911bd34dff3a5f37d
Yarik <arikpisarenko72@gmail.com>
2025-11-25 18:32:04 +0300

    добавить вычисления периметра прямоугольника

--- php/vars.php
+++ php/vars.php
@@ -8,3 +8,8 @@ $S = $dlina * $shirina;
 echo "длина " . $dlina . "<br>";
 echo "ширина" . $shirina . "<br>";
 echo "площадь прямоугольника" . $S . "<br>";
+$perimeter = ($dlina + $shirina) * 2;
+echo "периметр прямоугольника" . $perimeter . "<br>";
+echo "периметр прямоугольника с шириной " . $shirina
+    . " и длиной " . $dlina . " равен " . $perimeter . "конец"
+    . " а площадь равняется " . $S . "<br>";
