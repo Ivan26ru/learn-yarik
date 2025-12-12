2b988786f22245afacc8d4a62e46afb605f4ddda
Yarik <arikpisarenko72@gmail.com>
2025-12-12 18:48:31 +0300

    перенисти функции в одельный фаил

---
+++ php/functions.php
@@ -0,0 +1,18 @@
+<?php
+function perimeter(int $a, int $b)
+
+{
+    $perimeter = ($a + $b) * 2;
+    return $perimeter;
+}
+
+echo perimeter(3, 8) . "<br>";
+echo perimeter(2, 5) . "<br>";
+function plowabi(int $a, int $b)
+{
+    $plowabi = ($a * $b);
+    return $plowabi;
+}
+
+echo plowabi(5, 9) . "<br>";
+echo plowabi(9032, 398);
--- php/vars.php
+++ php/vars.php
@@ -19,4 +19,21 @@ $age = 10;
 $class = 4;
 echo " Привет " . $name
     . " мне " . $age . " лет "
-    . "я учусь в " . $class . " классе";
+    . "я учусь в " . $class . " классе <hr>";
+
+
+function perimeter(int $a, int $b)
+
+{
+    $perimeter = ($a + $b) * 2;
+    return $perimeter;
+}
+echo perimeter(3,8) . "<br>";
+echo perimeter(2,5) . "<br>";
+function plowabi (int $a, int $b)
+{
+  $plowabi = ($a * $b);
+  return $plowabi;
+}
+echo plowabi(5,9) . "<br>";
+ echo plowabi(9032,398);
