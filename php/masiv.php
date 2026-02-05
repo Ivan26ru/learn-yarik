0a6c0d96c89c4e3b3dc702aad04bcc59dedca953
Yarik <arikpisarenko72@gmail.com>
2026-02-05 13:24:18 +0300

    Сделал Д\З по курсу в папке masiv.php

---
+++ php/masiv.php
@@ -0,0 +1,42 @@
+<?php
+
+$person = [
+    "strana" => "Amerika",
+    " strana2" => "Egypt",
+    " strana3" => "China",
+    " strana4" => "Japan",
+];
+$person2 = [
+    "pet" => "dog",
+    "pet2" => "cat",
+    "pet3" => "hamster",
+    "pet4" => "Raccoon",
+];
+$person3 = [
+        "friend" => "Maxim",
+        "friend2" => "Gena",
+        "friend3" => "Ahmed",
+    ];
+ echo $person["strana"];
+ echo "<br>";
+ echo $person[" strana2"];
+ echo "<br>";
+  echo $person[" strana3"];
+  echo "<br>";
+   echo $person[" strana4"];
+   echo "<br>";
+
+   echo $person2["pet"];
+   echo "<br>";
+   echo $person2["pet2"];
+   echo "<br>";
+   echo $person2["pet3"];
+   echo "<br>";
+   echo $person2["pet4"];
+   echo "<br>";
+   echo $person3["friend"];
+   echo "<br>";
+   echo $person3["friend2"];
+   echo "<br>";
+   echo $person3["friend3"];
+   echo "<br>";
