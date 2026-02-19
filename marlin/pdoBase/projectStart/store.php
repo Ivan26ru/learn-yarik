cfec07fddb543f65d18c7498949fffa1b5ba867a
Yarik <arikpisarenko72@gmail.com>
2026-02-19 21:31:42 +0300

    Добавить пользователя в БД

---
+++ marlin/pdoBase/projectStart/store.php
@@ -0,0 +1,7 @@
+<?php
+$pdo = new PDO('mysql:host=MySQL-8.4;dbname=base', 'root', '');
+$sql = "INSERT INTO users (name,surname,username,email) VALUES (:name, :surname, :username, :email)";
+$statement = $pdo->prepare($sql);
+var_dump($_POST);
+$statement->execute($_POST);
+header("Location: /marlin/pdoBase/projectStart/");
--- marlin/pdoBase/projectStart/storn.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=MySQL-8.4;dbname=learn_yarik', 'root', '');
-$sql = "INSERT INTO users (username) VALUES (:username)";
-$statement = $pdo->prepare($sql);
-$statement->execute($_POST);
-header("Location:/index.php");
