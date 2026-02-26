9de1ddaeb335e7b5ce9d2dc3d696d1dbd033c563
Yarik <arikpisarenko72@gmail.com>
2026-02-26 21:41:44 +0300

    user  создаётся

--- marlin/pdoBase/projectStart/store.php
+++ marlin/pdoBase/projectStart/store.php
@@ -3,5 +3,5 @@ $pdo = new PDO('mysql:host=MySQL-8.4;dbname=base', 'root', '');
 $sql = "INSERT INTO users (name,surname,username,email) VALUES (:name, :surname, :username, :email)";
 $statement = $pdo->prepare($sql);
 var_dump($_POST);
-$statement->execute($_GET);
+$statement->execute($_POST);
 header("Location: /marlin/pdoBase/projectStart/");
