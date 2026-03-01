344db5316bacdef6cf8b6bd7d3db1e309db31651
Yarik <arikpisarenko72@gmail.com>
2026-03-01 21:36:27 +0300

    изменить пользователя из БД

--- marlin/pdoBase/projectStart/update.php
+++ marlin/pdoBase/projectStart/update.php
@@ -1,1 +1,9 @@
 <?php
+$pdo = new PDO('mysql:host=MySQL-8.4;dbname=base', 'root', '');
+$sql = "UPDATE users SET name=:name, surname=:surname, username=:username, email=:email WHERE id=:id";
+
+$statement = $pdo->prepare($sql);
+$statement->execute($_POST);
+header("Location: /marlin/pdoBase/projectStart/");
+
+?>
