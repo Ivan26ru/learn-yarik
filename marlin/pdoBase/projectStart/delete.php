6d9877823745275bc76370595776f127a406ec71
Yarik <arikpisarenko72@gmail.com>
2026-03-01 20:41:45 +0300

    испаравить  delete.php что-бы когда удалял учит запись перебрасовало на index.php

--- marlin/pdoBase/projectStart/delete.php
+++ marlin/pdoBase/projectStart/delete.php
@@ -1,6 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=MySQL-8.4;dbname=learn_yarik', 'root', '');
+$pdo = new PDO('mysql:host=MySQL-8.4;dbname=base', 'root', '');
 $sql = "DELETE FROM users WHERE ID = :id";
 $statement = $pdo->prepare($sql);
 $statement->execute($_GET);
 $user = $statement->fetch(PDO::FETCH_ASSOC);
+header("Location: index.php");
