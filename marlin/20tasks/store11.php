28cf875d23086af7a05f74e877e97c9d29c1e01d
Yarik <arikpisarenko72@gmail.com>
2026-04-07 22:47:57 +0300

    исправить редирект

--- marlin/20tasks/store11.php
+++ marlin/20tasks/store11.php
@@ -1,9 +1,10 @@
 <?php
 //$dataPost =["name"=>"dfsjkl"];
 $dataPost = $_POST;
-var_dump($dataPost);
+//var_dump($dataPost);
 $pdo = new PDO('mysql:host=MySQL-8.4;dbname=task_11', 'root', '');
 $sql = "INSERT INTO `text` (`name`) VALUES (:name)";
 $statement = $pdo->prepare($sql);
 $statement->execute($dataPost);
 header("Location: /marlin/20tasks/task_11.php");
+
