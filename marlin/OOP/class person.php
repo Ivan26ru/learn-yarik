cb61d3043f7da5f692704b49ed03c840fe483cdc
Yarik <arikpisarenko72@gmail.com>
2026-06-18 21:25:23 +0300

    просмотрел все видео

--- marlin/OOP/class person.php
+++ marlin/OOP/class person.php
@@ -25,8 +25,20 @@ class Person
  public function sayAge() {
     return $this->age;//14
 }
-$myPerson = new Person('Rahim',14);
-$myPerson2 = new Person('Alisa',10);
+public static function saySomething()
+{
+}
 
+class Student {
+    public $name = 'I am  student';
+}
+class University {
+    public $students;
+    public function addStudent(Student $student) {
+        echo $student->name;
+    }
+}
+$univer = new University;
 
-echo $myPerson : :ID;
+$person = new Person('Rahim', 20);
+$student->addStudent($person);
